37a0b5fcf334312404b0ce59fc21c45ddd90bc0d
Yeseul Song <chonsa0409@sookmyung.ac.kr>
2021-08-22 21:24:55 +0900

    2-6

---
+++ app.js
@@ -0,0 +1,19 @@
+// Log to console
+console.log('Hello world'); //text
+console.log(123);   // number
+console.log(true);  // bool
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]); // 배열
+console.table({a:1, b:2});  // 표
+
+console.error('This is some error'); // Error message
+console.clear();                    // delete console all.
+console.warn('This is a warning');  // warning message
+console.time('Hello');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+console.timeEnd('Hello'); // 다 작동하기까지 걸린 시간 측정.
