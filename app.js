0a3faca234a3392e105ac51416ae09af4fe8fc40
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-24 18:30:55 +0900

    2-8 Data types

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
-//var, let, const
+// PRIMITIVE - 변수에 저장
 
-var name = 'John Doe';
-console.log(name);
-name = "Song Yesuel";
-console.log(name);
-let namerevise;
-namerevise = 'Hansuel';
-console.log(namerevise);
+//String
+const name = 'John Doe';
+//Number
+const age = 45;
+// Boolean
+const hasKids = true;
+//Null
+const car = null;
+//undefined
+let test;
+// Symbol
+const sym = Symbol();
 
+//////////////////////////////////////////////
 
-//Init var
-var greeting;
-console.log(greeting); // Undefined
-greeting = 'Hey';
-console.log(greeting);
+// REFERENCE TYPES - Ojbects. (memory에 저장)
 
-//letters, numbers, _, $
-// Can not start with num
-
-//Multi word vars 
-var firstName = 'John'; // Camel case _ general
-var first_name = 'Song'; //underscore - 1,3번을 더 추천.
-var FirstName = 'Ye'; // Pascal case = similar to Camel
-// ㄴ get in to object oriented programming, when create constructor func.
-// ㄴ class with 6 classes.
-
-//CONST : constant. cna't change or can't be reassigned. 
-// if change, error message.  
-
-//But 변수 안에 요소들은 수정 가능.
-const person = {
-    name : 'John',
-    age : 30
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+    city : 'Boston',
+    state : 'MA'
 }
-person.name = 'Yesuel';
-console.log(person);
+const today = new Date();
 
-// array도 마찬가지.
-
-const numbers = [1,2,3,4,5];
-console.log(numbers);
-numbers.push(6);
-
-// '.' can change arrays &objets that are assigned to variables using const
-// const numbers = [1,2,3,4,5,6]; 이건 error. ('.'배열 전체 변경)
+console.log(today);
+console.log(typeof today);
