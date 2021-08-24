d6dd707e94350968875562a18459429e7088e786
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-24 18:06:12 +0900

    2-8

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-// Log to console
-console.log('Hello world'); //text
-console.log(123);   // number
-console.log(true);  // bool
-var greeting = 'Hello';
+//var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = "Song Yesuel";
+console.log(name);
+let namerevise;
+namerevise = 'Hansuel';
+console.log(namerevise);
+
+
+//Init var
+var greeting;
+console.log(greeting); // Undefined
+greeting = 'Hey';
 console.log(greeting);
-console.log([1,2,3,4]); // 배열
-console.table({a:1, b:2});  // 표
-//g
-console.error('This is some error'); // Error message
-console.clear();                    // delete console all.
-console.warn('This is a warning');  // warning message
-console.time('Hello');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-    console.log('Hello world');
-console.timeEnd('Hello'); // 다 작동하기까지 걸린 시간 측정.
+
+//letters, numbers, _, $
+// Can not start with num
+
+//Multi word vars 
+var firstName = 'John'; // Camel case _ general
+var first_name = 'Song'; //underscore - 1,3번을 더 추천.
+var FirstName = 'Ye'; // Pascal case = similar to Camel
+// ㄴ get in to object oriented programming, when create constructor func.
+// ㄴ class with 6 classes.
+
+//CONST : constant. cna't change or can't be reassigned. 
+// if change, error message.  
+
+//But 변수 안에 요소들은 수정 가능.
+const person = {
+    name : 'John',
+    age : 30
+}
+person.name = 'Yesuel';
+console.log(person);
+
+// array도 마찬가지.
+
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+numbers.push(6);
+
+// '.' can change arrays &objets that are assigned to variables using const
+// const numbers = [1,2,3,4,5,6]; 이건 error. ('.'배열 전체 변경)
