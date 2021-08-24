be7a3690b3e8a46f8a73e25f1ed7ba04f6988330
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-24 21:31:58 +0900

    2-10 Numbers & Math Object

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
+// 2-10 Numbers & Math Object
+
+const num1 = 10;
+const num2 = 2;
 let val;
 
-//Number to string
-val = String(555);
-val = String(4+4);
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;  // 몫
+val = num1 / num2;  //나머지
 
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1,2,3,4]);
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4); // 반올림
+val = Math.ceil(2,4); // 내림
+val = Math.floor(2.8); //올림
+val = Math.sqrt(64); // 루트
+val = Math.abs(-3); // 절댓값
+val = Math.pow(8,2); // 8^2 = 64
+val = Math.min(2,33,4,1,55,6,3,-2); // -2
+val = Math.max(2,33,4,1,55,6,3,-2); // 55
+val = Math.random();
 
-// toString()
-val = (5).toString();
-val = (true).toString();
 
-//Stirng to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello'); // NaN : 문자를 넘버로 할려고 할때. 안됨...
-val = Number([1,2,3]); // NaN
+val = Math.floor(Math.random()*20 + 1);
 
-val = parseInt('100.10'); // 정수만. 소수점 출력 안됨.
-val = perseFloat('100.10'); 
-
-//Output
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
-//console.log(val.length);
-console.log(val.toFixed()); // 원하는 소수점 자리까지 출력.
